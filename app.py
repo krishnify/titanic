9b730656ac5e0e360cf35cb0d3252e99d5ed3124
venkata krishna kota <krishnify@gmail.com>
2026-05-17 16:30:42 +0530

    addstreamlitapp

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
+import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Load dataset
-titanic = sns.load_dataset("titanic")
-
 
 print("Sadgurudeva")
 print("Ammayya")
@@ -12,17 +9,35 @@ print("Sainatha")
 print("Darbar Sainatha")
 print("Om Sam Saravanabhava")
 
+# Title
+st.title("Titanic Dataset Explorer 🚢")
+
+# Load dataset
+titanic = sns.load_dataset("titanic")
+
+st.write("### Raw Data Preview")
+st.dataframe(titanic.head())
+
 # Survival by gender
-sns.countplot(x="sex", hue="survived", data=titanic)
-plt.title("Survival by Gender")
-plt.show()
+st.write("### Survival by Gender")
+fig, ax = plt.subplots()
+sns.countplot(x="sex", hue="survived", data=titanic, ax=ax)
+st.pyplot(fig)
 
 # Survival by class
-sns.countplot(x="class", hue="survived", data=titanic)
-plt.title("Survival by Class")
-plt.show()
+st.write("### Survival by Class")
+fig, ax = plt.subplots()
+sns.countplot(x="class", hue="survived", data=titanic, ax=ax)
+st.pyplot(fig)
 
 # Age distribution
-sns.histplot(data=titanic, x="age", hue="survived", bins=30, kde=True)
-plt.title("Age Distribution by Survival")
-plt.show()
+st.write("### Age Distribution by Survival")
+fig, ax = plt.subplots()
+sns.histplot(data=titanic, x="age", hue="survived", bins=30, kde=True, ax=ax)
+st.pyplot(fig)
+
+# Fare vs survival
+st.write("### Fare vs Survival")
+fig, ax = plt.subplots()
+sns.boxplot(x="survived", y="fare", data=titanic, ax=ax)
+st.pyplot(fig)
